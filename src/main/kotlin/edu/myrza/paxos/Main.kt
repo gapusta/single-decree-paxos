fa524d06f69ee852ca41c108550c5c5626c03330
Курманбеков Мырзабек <m.kurmanbekov@wb-digital.ru>
2026-06-15 10:58:46 +0600

    Small and simple coroutine usage example

--- src/main/kotlin/edu/myrza/paxos/Main.kt
+++ src/main/kotlin/edu/myrza/paxos/Main.kt
@@ -3,12 +3,20 @@ package edu.myrza.paxos
 import edu.myrza.paxos.util.ShutdownHandler
 import io.vertx.core.Future
 import io.vertx.core.Vertx
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 fun main() {
-    start()
+    startCoroutinesExample()
+//    startSimulation()
 }
 
-fun start() {
+fun startSimulation() {
     val vertx = Vertx.vertx()
 
     val acceptors = listOf(
@@ -32,4 +40,33 @@ fun start() {
 
     // shutdown handler
     Runtime.getRuntime().addShutdownHook(ShutdownHandler(vertx))
+}
+
+fun startCoroutinesExample() = runBlocking {
+    launch {
+        // suspendCancellableCoroutine does 2 things:
+        // 1. calls the 'block' lambda. The 'block' makes the async operation callback to continue the coroutine when the operation is done
+        // (the callback calls continuation.resumeWith(Result.success(true)))
+        // 2. returns COROUTINE_SUSPENDED
+        CompletableFuture.supplyAsync {
+            Thread.sleep(2000)
+            true
+        }.apply {
+            suspendCancellableCoroutine<Boolean> { continuation ->
+                thenAccept {
+                    continuation.resume(it)
+                }
+                exceptionally {
+                    continuation.resumeWithException(it)
+                    null
+                }
+            }
+        }
+        println("hey #2")
+    }
+    launch {
+        delay(1000)
+        println("hey #3")
+    }
+    println("hey #1")
 }
